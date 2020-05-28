3093673f0c60657a64178216b794f0d92c15146f
Kasra Ferdowsifard <kferdowsifard@gmail.com>
2020-05-28 10:49:42 -0700

    .

---
+++ Main.java
@@ -0,0 +1,32 @@
+import pairpackage.Pair;
+
+class Game {
+    private int playerScore;
+    private int level;
+
+    Game(int playerScore, int level) {
+        this.playerScore = playerScore;
+        this.level = level;
+    }
+
+    void updateScore(int amount) {
+        this.playerScore = this.playerScore + amount;
+
+        if (this.playerScore < 0) {
+            this.playerScore = 0;
+        }
+    }
+
+    int getPlayerScore() {
+        return this.playerScore;
+    }
+}
+
+
+class Main {
+    public static void main(String[] args) {
+        Pair p = new Pair(4, 5);        
+
+        System.out.println(p.addComponents());
+    }
+}
